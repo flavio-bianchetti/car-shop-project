42310c8a2fc63458f8009b15134dfd70a6e33210
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-31 10:24:21 -0300

    feat: vehicleMiddleware removed

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,15 @@ import CustomRouter from './routes/router';
 import App from './app';
 
 import CarController from './controllers/Car';
-import VehicleMiddleware from './middlewares/Vehicle';
 
 import { Car } from './interfaces/CarInterface';
 
 const server = new App();
 
 const carController = new CarController();
-const vehicleMiddleware = new VehicleMiddleware();
 
 const carRouter = new CustomRouter<Car>();
-carRouter.addRoute(carController, vehicleMiddleware);
+carRouter.addRoute(carController);
 
 server.addRouter(carRouter.router);
 
