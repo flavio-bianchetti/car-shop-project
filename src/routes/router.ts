f1d6b77f24def4c87483734ac756323d09eb9ca0
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-31 10:24:56 -0300

    feat: Middleware removed

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import Controller from '../controllers';
-import Middleware from '../middlewares';
 
 class CustomRouter<T> {
   public router: Router;
@@ -11,12 +10,11 @@ class CustomRouter<T> {
 
   public addRoute(
     controller: Controller<T>,
-    middleware: Middleware<T>,
     route: string = controller.route,
   ) {
     this.router.get(route, controller.read);
     this.router.get(`${route}/:id`, controller.readOne);
-    this.router.post(route, middleware.validate, controller.create);
+    this.router.post(route, controller.create);
     this.router.put(`${route}/:id`, controller.update);
     this.router.delete(`${route}/:id`, controller.delete);
   }
