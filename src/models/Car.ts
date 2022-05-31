a3410d708585be13bd66aea845a3300f9639ef81
Flávio Bianchetti <fjb.developer@gmail.com>
2022-05-31 10:26:47 -0300

    feat: CarSchema versionKey exclude included

--- src/models/Car.ts
+++ src/models/Car.ts
@@ -4,15 +4,22 @@ import MongoModel from './MongoModel';
 
 interface CarDocument extends Car, Document {}
 
-const carSchema = new Schema<CarDocument>({
-  model: String,
-  year: Number,
-  status: String,
-  color: String,
-  buyValue: Number,
-  doorsQty: Number,
-  seatsQty: Number,
-});
+const carSchema = new Schema<CarDocument>(
+  {
+    model: String,
+    year: Number,
+    status: String,
+    color: String,
+    buyValue: Number,
+    doorsQty: Number,
+    seatsQty: Number,
+  },
+  {
+    // solução encontrada em:
+    // https://stackoverflow.com/questions/13699784/mongoose-v-property-hide
+    versionKey: false,
+  },
+);
 
 class CarModel extends MongoModel<Car> {
   constructor(model = createModel('Cars', carSchema)) {
